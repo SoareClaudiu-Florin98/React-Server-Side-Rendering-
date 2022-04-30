ed35962e0c651394b51c16329ecec10d1d5f9e1b
Soare Claudiu <soareclaudiuflorin@gmail.com>
2022-04-30 17:22:57 +0300

    load data

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ import path from 'path';
 import fs from 'fs';
 const app = express(); 
 app.use(express.static('./build', {index: false}))
+
+const articles = [
+    {title: "Article 1", author: "Bob"},
+    {title: "Article 2", author: "Betty"},
+    {title: "Article 3", author: "Frank"},
+]
+app.get('/api/articles', (req, res)=> {
+    const loadedArticles = articles ; 
+    res.json(loadedArticles);
+})
 app.get('/*', (req, res) => {
 
     const reactApp = renderToString(
--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export const Articles =() => {
-
+    const [articles, setArticles] = useState([]) ; 
+    useEffect(()=>{
+        fetch('/api/articles')
+        .then(response => response.json())
+        .then(data => setArticles(data))
+    },[])
     return (
         <>
         <h1>Articles</h1>
+        {articles && articles.map((article,index) => {
+            return(
+            <div key={index}>
+                <h3>{article.title}</h3>
+                <p>by {article.author}</p>
+            </div>);
+        })}
         </>
     )
 }
